73eeeb3cada558a8b0488de005cfe44eb41871f2
Classic <133490570+jearclaude1@users.noreply.github.com>
2024-05-20 13:02:35 +0200

    Css links

--- Pages/array.php
+++ Pages/array.php
@@ -32,7 +32,7 @@ if (isset($_POST["save"]))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../Assets/css/index.css">
     <title>Umucyo | Attendence</title>
 </head>
 <body>
--- Pages/update.php
+++ Pages/update.php
@@ -6,7 +6,7 @@ include("../php/connection/connection.php");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/index.css">
+    <link rel="stylesheet" href="../Asset/css/index.css">
     <title>Umucyo | Dashbord</title>
 </head>
 <body>
